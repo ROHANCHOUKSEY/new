07145b445c8538c7b3a8ce6bfd960e41badc7da1
smartpack5656-spec <smartpack5656@gmail.com>
2026-05-08 00:08:50 +0530

    Update abc function in getHello to accept three parameters for addition

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,11 +6,11 @@ export class AppService {
   constructor(){}
 
   getHello(): string{
-    function abc(a, b){
-        return a + b
+    function abc(a, b, c){
+        return a + b + c
     }
 
-    console.log(abc(2, 4))
+    console.log(abc(2, 4, 5))
 
     return 'Hello World!';
   }
